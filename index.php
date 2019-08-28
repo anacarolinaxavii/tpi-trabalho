15b988bcd37b2f939b304c2568b5e85447795c3b
Aluno <aluno@etec.sp.gov.br>
2019-08-28 14:38:13 -0300

    adicao conteceudo da index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="css/estilo.css" type="text/css" rel="stylesheet"/>
+    <head>
+        <body>
+            <div id="global">
+                <h1>GESCOLAR</h1>
+                <nav>
+                    <ul>
+                        <li><a href="cadastrar_alunos.php">Cadastrar Alunos </a></li>
+                        <li><a href="editar_alunos.php">editar alunos </a></li>
+
+</ul>
+</nav>
+</div>
+</body>
+    </html>
